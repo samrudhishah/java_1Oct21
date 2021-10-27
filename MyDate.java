aa7119273556dada51b3af3660de6c2c6640b4a6
samrudhishah <91862447+samrudhishah@users.noreply.github.com>
2021-10-27 19:08:37 +0530

    Add files via upload

---
+++ MyDate.java
@@ -0,0 +1,16 @@
+package labexercise1;
+
+public class MyDate {
+	int day=10,month=2,year=2021;
+void initDate()
+{
+	System.out.println(""+day+"/"+month+"/"+year);
+}
+	
+	public static void main(String[] args) {
+		
+MyDate d1=new MyDate();
+d1.initDate();
+	}
+
+}
